81a7ca47305517ce8c819dc68a1b8a219f565841
Neo no Jams <alecmatthews0109@gmail.com>
2020-04-29 18:25:46 +0700

    [+] Request #2 <Province name>

--- src/server.py
+++ src/server.py
@@ -2,6 +2,7 @@ import socket
 import threading
 import sys
 import json
+import unidecode
 
 # constant values
 PORT = 5050
@@ -11,6 +12,13 @@ ADDR = (HOST, PORT)
 HEADER = 64
 FORMAT = "utf-8"
 DISCONNECT_MSG = "!"
+
+INSTRUCTION = "[INSTRUCTION]\n1. Xem kết quả xổ số của tỉnh X, gửi truy vấn:\n                <X>           với X là tên tỉnh thành không có dấu, không có khoảng trắng.\n2. Tra sổ số theo số vé Y và tỉnh thành X, gửi truy vấn:\n      <X> <khoảng trắng> <Y>  với X là tên tỉnh thành không có dấu, không có khoảng trắng, Y là số vé của quý khách.\n"
+
+NOT_SUPPORT = "[NOT SUPPORT] Request is not supported. Please try again.\n"
+
+NO_PROVINCE = "[NOT FOUND] The province you looking up is not available. Please try again.\n"
+
 
 def create_socket():
     func_status = 1
@@ -90,7 +98,10 @@ def launch_db():
     province_mn = []
     for i in data_mn:
         location = i['location']
-        province_mn.append(location)
+        if location not in province_mn:
+            province_mn.append(location)
+        else:
+            data_mn.remove(i)
 
 
     # Data from mien Trung
@@ -102,10 +113,13 @@ def launch_db():
     province_mt = []
     for i in data_mt:
         location = i['location']
-        province_mt.append(location)
+        if location not in province_mt:
+            province_mt.append(location)
+        else:
+            data_mt.remove(i)
 
 
-    # Data from mien Trung
+    # Data from mien Bac
     with open('crawl_data/db/xsmb.json') as json_file:
         json_data = json_file.read()
 
@@ -114,36 +128,80 @@ def launch_db():
     province_mb = []
     for i in data_mb:
         location = i['location']
-        province_mb.append(location)
+        if location not in province_mb:
+            province_mb.append(location)
+        else:
+            data_mb.remove(i)
 
     province_list = [province_mb, province_mt, province_mn]
     data_list = [data_mb, data_mt, data_mn]
     
     return province_list, data_list
 
+def handle_VNese(string):
+    new_string = unidecode.unidecode(string)
+    new_string = new_string.replace(" ", "")
+    return new_string.lower()
 
 def handle_request(msg, province_list, data_list):
-    if msg == 'h':
-        instruction = "[INSTRUCTION]\n1. Xem kết quả xổ số của tỉnh X, gửi truy vấn:\n                <X>           với X là tên tỉnh thành không có dấu, không có khoảng trắng.\n2. Tra sổ số theo số vé Y và tỉnh thành X, gửi truy vấn:\n      <X> <khoảng trắng> <Y>  với X là tên tỉnh thành không có dấu, không có khoảng trắng, Y là số vé của quý khách.\n\n"
+    msg = msg.lower()
+    msg = msg.strip(" ")
+    args_count = len(msg.split())
 
-        province_string = "[LIST] Danh sách các tỉnh có mở thưởng:\n"
-        count = 0
-        for region in province_list:
-            if count == 0:
-                province_string += "+ Miền Bắc:\n"
-            elif count == 1:
-                province_string += "+ Miền Trung:\n"
-            elif count == 2:
-                province_string += "+ Miền Nam:\n"
-            else:
-                break
+    if msg.isdigit():
+        return NOT_SUPPORT
 
-            for province in region:
-                province_string += ("    " + province + "\n")
+    if args_count == 1:
+        if msg == 'h':
+            output_string = "[RESPONSE] Danh sách các tỉnh có mở thưởng:\n"
+            count = 0
+            for region in province_list:
+                if count == 0:
+                    output_string += "+ Miền Bắc:\n"
+                elif count == 1:
+                    output_string += "+ Miền Trung:\n"
+                elif count == 2:
+                    output_string += "+ Miền Nam:\n"
+                else:
+                    break
 
-            count += 1
+                for province in region:
+                    output_string += ("    " + province + "\n")
+                count += 1
+            return INSTRUCTION + "\n" + output_string
+        else:
+            output_string = "[RESPONSE] Kết quả xổ số của tỉnh "
+            flag = 0
+            found = 0
+            province_to_search = ""
 
-        return instruction + province_string
+            for region in province_list:
+                for province in region:
+                    if msg == handle_VNese(province):
+                        province_to_search = province
+                        found = 1
+                        break
+                if found == 1:
+                    break
+                flag += 1
+
+            if flag < 3:
+                output_string += (province_to_search + ":\n")
+                data_to_search = data_list[flag]
+                for block in data_to_search:
+                    if block['location'] == province_to_search:
+                        for reward in block['data']:
+                            output_string += ("     " + reward + ": " + block['data'][reward] + "\n")
+                        return output_string            
+            return NO_PROVINCE
+                 
+    elif args_count == 2:
+        #Request 2
+        province_request = msg.split()[0]
+        return ""
+    
+    else:
+        return NOT_SUPPORT
 
 def main():
     province_list, data_list = launch_db()
