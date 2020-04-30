dd653fb75e370f45482b7bef3c8cd5e3e1f7339c
Neo no Jams <alecmatthews0109@gmail.com>
2020-04-30 19:04:03 +0700

    [*] Update database

--- src/server.py
+++ src/server.py
@@ -238,20 +238,22 @@ def handle_request(msg, province_list, data_list, reward_value):
     
     return NOT_SUPPORT
 
-def main():
-    print("[UPDATING] Database is being updated...\n")
-
-    with open("temp_log.txt", "a") as f:
-        crawl = subprocess.run("del crawl_data\\db\\xsmb.json crawl_data\\db\\xsmt.json crawl_data\\db\\xsmn.json && cd crawl_data && scrapy crawl", stdout=f, stderr=f, text=True, shell=True)
-
-    print("[LOADING] Loading database to serve...\n")
-    province_list, data_list, reward_value = launch_db()
-
-    print("[DONE] Data is loaded.\n")
-
+def main(): 
     func_status, server = create_socket()
+
     if func_status:
+        print("[UPDATING] Database is being updated...\n")
+        with open("temp_log.txt", "a") as f:
+            crawl = subprocess.run("del crawl_data\\db\\xsmb.json crawl_data\\db\\xsmt.json crawl_data\\db\\xsmn.json && cd crawl_data && scrapy crawl", stdout=f, stderr=f, text=True, shell=True)
+
+        print("[LOADING] Loading database to serve...\n")
+        province_list, data_list, reward_value = launch_db()
+
+        print("[DONE] Data is loaded. Ready to serve.\n")
+
         print("[STARTING] Server is starting...\n")
         start(server, province_list, data_list, reward_value)
     else:
         print("[SHUTTING DOWN] Closing application due to create socket error...")
+
+    server.close()
